76f85a52b7ef15b492090ae9cb50c5dfb188e95b
KwameFidel <88569241+KwameFidel@users.noreply.github.com>
2021-11-17 23:45:58 +0000

    Update App.js

--- bike-app/App.js
+++ bike-app/App.js
@@ -6,7 +6,9 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Interface from "./screens/Interface";
 import Home from "./screens/Home";
 
+import Cart from './screens/Cart';
 import { theme } from './core/theme'
+
 import {
   StartScreen,
   LoginScreen,
@@ -15,9 +17,9 @@ import {
   Dashboard,
 } from './screens';
 
-import {MaleScreen} from './screens/MaleScreen';
-import {FemaleScreen} from './screens/FemaleScreen';
-import {KidsScreen} from './screens/KidsScreen';
+
+
+
  
 
 export default function App() {
@@ -40,9 +42,7 @@ export default function App() {
       
           <MainNavigator.Screen name="StartScreen" component={StartScreen} />
           <MainNavigator.Screen name="RegisterScreen" component={RegisterScreen} />
-          <MainNavigator.Screen name="MaleScreen" component={MaleScreen} />
-          <MainNavigator.Screen name="FemaleScreen" component={FemaleScreen} />
-          <MainNavigator.Screen name="KidsScreen" component={KidsScreen} />
+          <MainNavigator.Screen name ="Cart" component={Cart} />
         </MainNavigator.Navigator>
       </NavigationContainer>
     </View>
